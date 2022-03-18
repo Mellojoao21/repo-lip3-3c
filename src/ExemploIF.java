8cbccc429adbd80ffaad8e0e99befd3f7da9b37e
João Victor Leal Almeida de Mello <rm79188@estudante.fieb.edu.br>
2022-03-18 09:07:32 -0300

    Primeiro Commit

---
+++ src/ExemploIF.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+public class ExemploIF {
+
+	public static void main(String[] args) {
+		
+		int x = 10;
+		int y = 5;
+		
+		if( x > y ) {
+			JOptionPane.showMessageDialog(null, x + " é maior que " + y);
+		} else if ( x < y ) { 
+			JOptionPane.showMessageDialog(null, x + " é menor que " + y);
+		} else { 
+			JOptionPane.showMessageDialog(null, x + " é igaul a " + y);
+			
+	}			
+		
+	}
+
+}
